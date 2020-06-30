f001598f976a6906296d30c239d1250b30d05ad5
serif <vrenna@live.com>
2020-06-29 22:44:56 -0400

    git commit fix

--- diedummydie_fix.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-git filter-branch -f --env-filter "
-    GIT_AUTHOR_NAME='serif'
-    GIT_AUTHOR_EMAIL='vrenna@live.com'
-    GIT_COMMITTER_NAME='serif'
-    GIT_COMMITTER_EMAIL='vrenna@live.com'
-  " HEAD
